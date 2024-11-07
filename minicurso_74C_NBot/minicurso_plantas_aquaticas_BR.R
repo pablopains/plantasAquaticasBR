07eb293e9e2f68c310061a21ce53a5303bb4c3eb
Pablo Hendrigo Alves de Melo <pablopains@yahoo.com.br>
2024-11-07 20:06:36 -0300

    Update minicurso_plantas_aquaticas_BR.R

--- minicurso_74C_NBot/minicurso_plantas_aquaticas_BR.R
+++ minicurso_74C_NBot/minicurso_plantas_aquaticas_BR.R
@@ -1,5 +1,12 @@
+#' @title Plantas Aquáticas em R - 74 Congresso Nacional de Botânica - Brasília,
+#' @author Pablo Hendrigo Alves de Melo
+#' @details 
+#' pablopains@yahoo.com.br
 
-### plantas aquáticas do Brasil
+
+### Roteiro para prárica
+
+### Plantas Aquáticas do Brasil
 
   # https://sites.icb.ufmg.br/plantasaquaticasbrasil/index.htm
 
@@ -7,21 +14,39 @@
 
   # https://github.com/pablopains/plantasAquaticasBR
 
+  # github - instalação de pacotes
   # https://github.com/PlantasAquaticasBrasil/plantasAquaticasBR  
 
 
 ### Catálogo de Plantas das UCs do Brasil
 
+  # Catalogo 
   # https://catalogo-ucs-brasil.jbrj.gov.br/
   
+  # APPs online
   # https://pablopains.shinyapps.io/catalogoUCsBR_prepare/
     
   # https://pablopains.shinyapps.io/catalogoUCsBR_review/
 
   # https://pablopains.shinyapps.io/catalogoUCsBR_publication/
   
+  # github - instalação de pacotes
   # https://github.com/pablopains/catalogoUCsBR
-  
+
+  # link dados
+  # REFLORA
+  # https://github.com/pablopains/plantasAquaticasBR/blob/main/minicurso_74C_NBot/REFLORA-Pains_RelatorioConsultaTestemunho.xlsx  
+
+  # JABOT
+  # https://github.com/pablopains/plantasAquaticasBR/blob/main/minicurso_74C_NBot/Jabot_Geral_DarwinCore_Pains.csv
+
+  # SPLink
+  # https://github.com/pablopains/plantasAquaticasBR/blob/main/minicurso_74C_NBot/speciesLink-20241107185859-0005493_Pains.txt
+  # na fonte: https://specieslink.net/search/download/20241107185859-0005493
+
+  # GBIF
+  # https://github.com/pablopains/plantasAquaticasBR/blob/main/minicurso_74C_NBot/GBIF_occurrence_Pains.txt
+  # na fonte: https://doi.org/10.15468/dl.8wc537
 
   install.packages('devtools')
   devtools::install_github("pablopains/catalogoUCsBR", dependencies = TRUE)
@@ -29,10 +54,13 @@
   
 ### parseGBIF
   
+  # APP online
   #  https://pablopains.shinyapps.io/parseGBIF/
   
+  # github - instalação de pacotes
   # https://github.com/pablopains/parseGBIF
   
+  # link dados
   # https://doi.org/10.15468/dl.8wc537
   
   
