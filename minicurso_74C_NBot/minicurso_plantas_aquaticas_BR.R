608f5c3c79b8b6828251c8632ac47195725dabe5
Pablo Hendrigo Alves de Melo <pablopains@yahoo.com.br>
2024-11-08 10:55:14 -0300

    palestra

--- minicurso_74C_NBot/minicurso_plantas_aquaticas_BR.R
+++ minicurso_74C_NBot/minicurso_plantas_aquaticas_BR.R
@@ -4,7 +4,21 @@
 #' pablopains@yahoo.com.br
 
 
-### Roteiro para prárica
+### Roteiro para prárica ###
+
+### Instalção de programas
+# R 4
+# https://cran.r-project.org/bin/windows/base/R-4.4.2-win.exe
+ 
+ 
+# R Tools 4
+# https://cran.r-project.org/bin/windows/Rtools/rtools44/files/rtools44-6335-6327.exe
+ 
+ 
+# RStudio
+# https://download1.rstudio.org/electron/windows/RStudio-2024.09.1-394.exe
+ 
+
 
 ### Plantas Aquáticas do Brasil
 
@@ -48,9 +62,23 @@
   # https://github.com/pablopains/plantasAquaticasBR/blob/main/minicurso_74C_NBot/GBIF_occurrence_Pains.txt
   # na fonte: https://doi.org/10.15468/dl.8wc537
 
-  install.packages('devtools')
-  devtools::install_github("pablopains/catalogoUCsBR", dependencies = TRUE)
 
+  install.packages('writexl')
+  install.packages('tidyverse')
+  install.packages('measurements')
+  install.packages('readxl')
+  install.packages('shinydashboardPlus')
+  install.packages( 'writexl')
+
+  
+  install.packages('devtools')
+  devtools::install_github("pablopains/catalogoUCsBR")
+  
+  catalogoUCsBR::app_prepare()
+  catalogoUCsBR::app_review()
+  catalogoUCsBR::app_publication()
+    
+  
   
 ### parseGBIF
   
@@ -63,10 +91,11 @@
   # link dados
   # https://doi.org/10.15468/dl.8wc537
   
+  install.packages('jsonify')
   
   install.packages('devtools')
   devtools::install_github("pablopains/parseGBIF", dependencies = TRUE)
 
-
+  parseGBIF::parseGBIF_app()
     
   
